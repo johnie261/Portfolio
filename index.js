1337b2c3d998a1f11bb96ca2b1dc678823e9fbf8
johnie261 <njorogejohn261@gmail.com>
2023-01-31 01:38:37 +0300

    Make changes on mobile menu

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const menu = document.querySelector('#menuBttn');
 const navbar = document.querySelector('.navbar');
+const logo = document.querySelector('.headr .logo .log');
 
 menu.addEventListener('click', () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
+    logo.classList.toggle('active');
+
 });
 
 
